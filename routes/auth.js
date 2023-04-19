54a295d4dce52e3c70e1cf8613058a9907830f98
Rahul Kumar Pandey <rahulkrpandey221@.com>
2023-04-19 22:23:50 +0530

    Added bcrypt library to hash password

--- routes/auth.js
+++ routes/auth.js
@@ -1,51 +1,54 @@
-const router = require('express').Router();
+const router = require('express').Router(); const bcrypt = require('bcrypt');
+
 const Customer = require('../models/customer');
 
-const authMiddleware = (req, res, next) => {
-    // console.log("here");
-    if (req.method === 'POST') {
+router.post('/signup', async (req, res) => {
+    const dataValidatorUtil = (req, res) => {
         // checking if every field is valid and present or not
-
         const body = req.body;
 
         if (body.name === undefined || body.name.length === 0) {
-            res.status(400).send("name is not valid");
-            return;
+            throw new Error("Name is required");
         }
 
         if (body.email === undefined || body.email.length === 0) {
-            res.status(400).send("email is not valid");
-            return;
+            throw new Error("Email is required");
         }
 
         if (body.password === undefined || body.password.length === 0) {
-            res.status(400).send("password is not valid");
-            return;
+            throw new Error("Password is required");
         }
 
         if (body.address === undefined || body.address.length === 0) {
-            res.status(400).send("address is not valid");
-            return;
+            throw new Error("Address is required");
         }
 
         if (body.contact === undefined || body.contact.toString().length === 0) {
-            res.status(400).send("contact is not valid");
+            throw new Error("Contact is required");
+        }
+    };
+
+    try {
+        dataValidatorUtil(req, res);
+    } catch (err) {
+        res.status(400).send(err.toString());
+        return;
+    }
+
+    console.log("I am here");
+    const salt = 10;
+    bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal Server Error");
             return;
         }
 
-    }
-
-    next();
-};
-
-router.use(authMiddleware);
-
-router.post('/signup', async (req, res) => {
-    try {
         const customer = new Customer({
             name: req.body?.name,
             email: req.body?.email,
-            password: req.body?.password,
+            // password: req.body?.password,
+            password: hashedPassword,
             address: req.body?.address,
             isAdmin: false,
             likes: [],
@@ -55,14 +58,72 @@ router.post('/signup', async (req, res) => {
 
         await customer.save();
         res.status(201).json(customer);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error");
-    }
+    });
 });
 
+// Login route 
 router.post('/login', async (req, res) => {
-    const body = req.body;
+    const dataValidatorUtil = (req, res) => {
+        // checking if every field is valid and present or not
+        const body = req.body;
+
+        if (body.email === undefined || body.email.length === 0) {
+            throw new Error("Email is required");
+        }
+
+        if (body.password === undefined || body.password.length === 0) {
+            throw new Error("Password is required");
+        }
+    };
+
+    try {
+        dataValidatorUtil(req, res);
+    } catch (err) {
+        return res.status(400).send(err.toString());
+    }
+
+    // function to find customer with given email
+    const findCustomer = async (email) => {
+        try {
+            const customer = await Customer.findOne({
+                email: email
+            });
+
+            return customer;
+        } catch (err) {
+            throw err;
+        }
+    };
+
+    // function to compare password
+    const passwordChecker = async (hashedPassword) => {
+        try {
+            const result = await bcrypt.compare(req.body.password, hashedPassword);
+            return result;
+        } catch (err) {
+            throw err;
+        }
+    };
+
+    try {
+        const customer = await findCustomer(req.body.email);
+
+        if (customer === null) {
+            return res.status(401).send("Wrong email");
+        }
+
+        const result = await passwordChecker(customer.password);
+
+        if (result === true) {
+            res.status(200).send("Logged in successfully");
+            return;
+        } else {
+            res.status(400).send("Wrong password");
+        }
+    } catch (err) {
+        res.status(500).send("Internal server error");
+    }
+
 });
 
 module.exports = router;
