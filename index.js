f02e048cb533fb2fdcf62f6ef7036989a403b33f
Rahul Kumar Pandey <rahulkrpandey221@.com>
2023-04-28 22:25:53 +0530

    added routes to handle error

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const mongoose = require('mongoose');
 
 app.use(express.json());
 
-app.use((err, req, res, next) => {
-    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-        console.error('Invalid JSON:', err.message);
-        res.status(400).send('Invalid JSON');
-    } else {
-        next();
-    }
-});
+// app.use((err, req, res, next) => {
+//     if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+//         console.error('Invalid JSON:', err.message);
+//         res.status(400).send('Invalid JSON');
+//     } else {
+//         next();
+//     }
+// });
 
 mongoose.connect(process.env.MONGODB_CONNECT).then(() => {
     console.log("db connected");
@@ -30,9 +30,19 @@ app.use('/api/category', categoryRouter);
 app.use('/api/product', productRouter);
 app.use('/api/order', orderRouter);
 
-app.get('/', (req, res) => {
+app.get('/api/', (req, res) => {
     res.status(200).send('Shoptronics api');
 });
+
+app.use((req, res, next) => {
+    const err = new Error('Not found!');
+    err.status = 400;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.status).send(err.message);
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`server started ${process.env.PORT}`);
