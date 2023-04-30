bd43a32a3bcda8b5455525bb5d316f4eaaab9d7e
Rahul Kumar Pandey <rahulkrpandey221@gmail.com>
2023-05-01 00:31:44 +0530

    updated all routes in product.js route

--- routes/product.js
+++ routes/product.js
@@ -1,94 +1,29 @@
 const router = require('express').Router();
-const Customer = require('../models/customer');
 const Product = require('../models/product');
-const jwt = require('jsonwebtoken');
 
-const authorizeTokenUtil = async (req) => {
-    const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null;
-    if (token === null) {
-        throw new Error("You are not authorized, token not found");
-    }
+const { verifyAdmin, verifyToken } = require('../middlewares/auth');
+const { findCustomerWithId, findProduct, findProductsByCategory } = require('../utils/utils');
 
+router.post('/', verifyToken, verifyAdmin, async (req, res) => {
     try {
-        const data = await jwt.verify(token, process.env.JWT_KEY);
-        return data;
-    } catch (err) {
-        throw err;
-    }
-};
-
-const findUser = async (id) => {
-    try {
-        const user = await Customer.findOne({
-            _id: id
-        })
-
-        return user;
-    } catch (err) {
-        throw err;
-    }
-};
-
-const findProduct = async (id) => {
-    try {
-        const product = await Product.findOne({
-            _id: id
-        })
-
-        return product;
-    } catch (err) {
-        throw err;
-    }
-};
-
-const findProductsByCategory = async (category, count) => {
-    try {
-        const products = await Product.find({
-            categories: {
-                $in: [category]
-            }
-        }).limit(count);
-
-        return products;
-    } catch (err) {
-        throw err;
-    }
-};
-
-router.post('/', async (req, res) => {
-    try {
-        const data = await authorizeTokenUtil(req);
-        const admin = await findUser(data.id);
-
-        if (!admin || admin.isAdmin === false) {
-            throw new Error("You are not authorized");
-        }
-
-        const body = req.body;
+        const _product = req.body.product;
         const product = new Product({
-            name: body.name,
-            categories: body.categories,
-            description: body.description,
-            likes: body.likes,
-            price: body.price,
+            name: _product.name,
+            categories: _product.categories,
+            description: _product.description,
+            likes: _product.likes,
+            price: _product.price,
         });
 
         await product.save();
         res.status(201).json(product);
     } catch (err) {
-        res.status(400).json(err);
+        res.status(err.status || 500).send(err.message || "Internal server error");
     }
 });
 
-router.get('/', async (req, res) => {
+router.get('/', verifyToken, async (req, res) => {
     try {
-        const data = await authorizeTokenUtil(req);
-        const user = await findUser(data.id);
-
-        if (!user) {
-            throw new Error("You are not authorized");
-        }
-
         const query = req.query;
 
         if (query.id) {
@@ -100,78 +35,49 @@ router.get('/', async (req, res) => {
         const count = query.count ? +query.count : 10;
 
         const products = await findProductsByCategory(category, count);
-        console.log(category + " " + count);
-        console.log(products);
         res.status(200).json(products);
     } catch (err) {
-        res.status(500).send("Internal server error");
+        res.status(err.status || 500).send(err.message || "Internal server error");
     }
 });
 
-router.patch('/likes', async (req, res) => {
+router.patch('/likes', verifyToken, async (req, res) => {
     try {
-        const data = await authorizeTokenUtil(req);
-        const user = await findUser(data.id);
+        const user = await findCustomerWithId(req.headers.userId);
 
         const productId = req.query.id;
         const product = await findProduct(productId);
-        if (product === null) {
-            return res.status(400).send("Product not found");
-        }
 
-        console.log(user);
         user.likes.push(productId);
-        await user.save();
 
         product.likes++;
-        console.log(product);
-        await product.save();
+        await Promise.all([product.save(), user.save()]);
 
         res.status(201).json(product);
     } catch (err) {
-        res.status(400).send(err);
+        res.status(err.status || 500).send(err.message || "Internal server error");
     }
 });
 
-router.patch('/', async (req, res) => {
+router.patch('/', verifyToken, verifyAdmin, async (req, res) => {
     try {
-        const data = await authorizeTokenUtil(req);
-        const admin = await findUser(data.id);
-
-        if (!admin.isAdmin) {
-            return res.status(400).send("You are not authorized");
-        }
-
         const productId = req.query.id;
         const product = await findProduct(productId);
-        if (product === null) {
-            return res.status(400).send("Product not found");
-        }
 
-        const body = req.body;
+        const _product = req.body.product;
         await Product.updateOne({
             _id: productId
-        }, { ...body });
+        }, { ..._product });
 
         res.status(201).json(await findProduct(product._id));
     } catch (err) {
-        res.status(400).send(err);
+        res.status(err.status || 500).send(err.message || "Internal server error");
     }
 });
 
-router.delete('/', async (req, res) => {
+router.delete('/', verifyToken, verifyAdmin, async (req, res) => {
     try {
-        const data = await authorizeTokenUtil(req);
-        const admin = await findUser(data.id);
-
-        if (!admin.isAdmin) {
-            return res.status(400).send("You are not authorized");
-        }
-
         const product = await findProduct(req.query.id);
-        if (product === null) {
-            return res.status(400).send("Product not found");
-        }
 
         await Product.deleteOne({
             _id: req.query.id,
@@ -179,7 +85,7 @@ router.delete('/', async (req, res) => {
 
         res.status(201).json(product);
     } catch (err) {
-        res.status(400).send(err);
+        res.status(err.status || 500).send(err.message || "Internal server error");
     }
 });
 
